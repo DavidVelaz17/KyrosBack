475263533526e19f983202f42cf0a1f2a56eb892
DavidVelaz17 <david.lara.velaz@gmail.com>
2026-06-22 18:28:05 -0600

    project initialization

---
+++ src/main/java/com/kyros/demokyros/DemokyrosApplication.java
@@ -0,0 +1,13 @@
+package com.kyros.demokyros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemokyrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemokyrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kyros/demokyros/DemokyrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyros.demokyros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemokyrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
